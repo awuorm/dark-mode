770768560090ceff926e30b3bce6a94a7d981621
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-03 08:36:39 +0300

    created dropdown list

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Chart from "./Chart";
 
-const Charts = (props) => {
-  const {coinData, coinDropDown} = props;
+const Charts = props => {
+  const { coinData, coinDropDown } = props;
+  console.log("coins from index", coinDropDown);
   return (
     <div className="charts">
-      
-      {coinData.map(coin => (
-        <div className="chart__container" key={coin.name}>
-          <h2 className="coin__title">{coin.name}</h2>
-          <h4 className="coin__symbol">{coin.symbol}</h4>
-          <div className="coin__logo">
-            <img src={coin.image} height="40" alt={coin.name} />
+      { Array.isArray(coinDropDown) ?   (
+        coinDropDown.map(coin => (
+          <div className="chart__container" key={coin.name}>
+            <h2 className="coin__title">{coin.name}</h2>
+            <h4 className="coin__symbol">{coin.symbol}</h4>
+            <div className="coin__logo">
+              <img src={coin.image} height="40" alt={coin.name} />
+            </div>
+            <Chart sparklineData={coin.market_data.sparkline_7d.price} />
           </div>
-          <Chart sparklineData={coin.sparkline_in_7d.price} />
+        ))
+      ) : (
+        <div className="chart__container" key={coinDropDown.name}>
+          <h2 className="coin__title">{coinDropDown.name}</h2>
+          <h4 className="coin__symbol">{coinDropDown.symbol}</h4>
+          <div className="coin__logo">
+            <img src={coinDropDown.image} height="40" alt={coinDropDown.name} />
+          </div>
+          <Chart sparklineData={coinDropDown.market_data.sparkline_7d.price} />
         </div>
-      ))}
+      )  }
     </div>
   );
 };
